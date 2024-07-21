cdc7db0dcef0a1624014166f1d46ef195ed56fb7
hee9841 <lee98417716@gmail.com>
2024-07-21 20:02:29 +0900

    feat: JWTFilter를 통과한 뒤 세션 정보
    - 일시적으로 세션을 가지고 있음
    - 아이디, 역활 정보 빼오기

--- src/main/java/com/example/security/controller/AdminController.java
+++ src/main/java/com/example/security/controller/AdminController.java
@@ -1,6 +1,11 @@
 package com.example.security.controller;
 
+import java.util.Collection;
+import java.util.Iterator;
 import lombok.Getter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,6 +16,18 @@ public class AdminController {
 
     @GetMapping("/admin")
     public String adminP() {
-        return "Admin Controller";
+        //현재 사용자 id
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        //현재 사용자 role
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+        GrantedAuthority auth = iter.next();
+        String role = auth.getAuthority();
+
+
+        return "Admin Controller : " + name + ", " + role;
     }
 }
--- src/main/java/com/example/security/controller/MainController.java
+++ src/main/java/com/example/security/controller/MainController.java
@@ -1,5 +1,10 @@
 package com.example.security.controller;
 
+import java.util.Collection;
+import java.util.Iterator;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
